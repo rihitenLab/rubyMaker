f98b805773033d58af340039ed59a3da3adf9e9a
rihitenLab <rihitenLab@gmail.com>
2019-04-14 01:18:02 +0900

    Create xcode project

---
+++ rubyMaker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  rubyMaker
+//
+//  Created by ri-matsuoka on 2019/04/14.
+//  Copyright © 2019年 ri-matsuoka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
